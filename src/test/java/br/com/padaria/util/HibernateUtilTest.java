e477f8d665e40f872bee96f4682407893f366adc
andreluis7 <48221325+andreluis7@users.noreply.github.com>
2020-06-06 16:59:01 -0300

    (#4) Adicao dos arquivos para execucao do projeto

---
+++ src/test/java/br/com/padaria/util/HibernateUtilTest.java
@@ -0,0 +1,13 @@
+package br.com.padaria.util;
+
+import org.hibernate.Session;
+import org.junit.Test;
+
+public class HibernateUtilTest {
+	@Test
+	public void conectar(){
+		Session sessao = HibernateUtil.getFabricaDeSessoes().openSession();
+		sessao.close();
+		HibernateUtil.getFabricaDeSessoes().close();
+	}
+}

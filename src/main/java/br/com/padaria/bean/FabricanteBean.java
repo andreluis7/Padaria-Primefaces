252994b43ca03a14efd45f2818601acb7ad600e9
andreluis7 <48221325+andreluis7@users.noreply.github.com>
2020-06-13 18:38:04 -0300

    (#13) Ajustes fabricante

--- src/main/java/br/com/padaria/bean/FabricanteBean.java
+++ src/main/java/br/com/padaria/bean/FabricanteBean.java
@@ -1,21 +1,14 @@
 package br.com.padaria.bean;
 
 import java.io.Serializable;
-import java.util.Arrays;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.event.ActionEvent;
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.client.WebTarget;
 
 import org.omnifaces.util.Messages;
-
-import com.google.gson.Gson;
 
 import br.com.padaria.dao.FabricanteDAO;
 import br.com.padaria.domain.Fabricante;
@@ -45,15 +38,48 @@ public class FabricanteBean implements Serializable {
 
 	@PostConstruct
 	public void listar() {
-
+		try {
+			FabricanteDAO fabricanteDAO = new FabricanteDAO();
+			fabricantes = fabricanteDAO.listar("codigo");
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Ocorreu um erro ao tentar listar os fabricantes");
+			erro.printStackTrace();
+		}
 	}
 
 	public void novo() {
-		fabricante = new Fabricante();
+		try {
+			fabricante = new Fabricante();
+
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Ocorreu um erro ao tentar gerar um novo fabricante");
+			erro.printStackTrace();
+		}
+	}
+
+	public void editar(ActionEvent evento) {
+		try {
+			fabricante = (Fabricante) evento.getComponent().getAttributes().get("fabricanteSelecionado");
+
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Ocorreu um erro ao tentar selecionar um fabricante");
+		}
 	}
 
 	public void salvar() {
-		
+		try {
+			FabricanteDAO fabricanteDAO = new FabricanteDAO();
+			fabricanteDAO.merge(fabricante);
+
+			fabricantes = fabricanteDAO.listar("codigo");
+
+			fabricante = new Fabricante();
+
+			Messages.addGlobalInfo("Fabricante salvo com sucesso");
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Ocorreu um erro ao tentar salvar o fabricante");
+			erro.printStackTrace();
+		}
 	}
 
 	public void excluir(ActionEvent evento) {
@@ -65,14 +91,11 @@ public class FabricanteBean implements Serializable {
 
 			fabricantes = fabricanteDAO.listar();
 
-			Messages.addGlobalInfo("Fabricante removido com sucesso");
-		} catch (RuntimeException erro) {
+			Messages.addGlobalInfo("Produto removido com sucesso");
+		} catch (Exception erro) {
 			Messages.addFlashGlobalError("Ocorreu um erro ao tentar remover o fabricante");
 			erro.printStackTrace();
 		}
 	}
 
-	public void editar(ActionEvent evento) {
-		fabricante = (Fabricante) evento.getComponent().getAttributes().get("fabricanteSelecionado");
-	}
 }
